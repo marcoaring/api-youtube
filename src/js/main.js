e0495f66bc6e18e2216852297cb7366134a90ccd
Marco Aring <contato@marcoaring.com.br>
2018-10-19 23:21:30 -0300

    feature/FV-01 - Desenvolvimento do header do site

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,36 @@
-$(document).ready(function(){
+var appVue = new Vue({
+    el: ".app-vue",
+    data: {
+        searchResults: [],
+        menuSelected: false,
+        searchSelected: false,
+        search: '',
+    },
+
+    created: function(){
+        //this.loadPosts();
+    },
+
+    methods:{
+        openMenu: function(){
+            if(this.menuSelected){
+            	this.menuSelected = false;
+            } else{
+            	this.menuSelected = true;
+                this.searchSelected = false;
+            }
+        },
+
+        openSearch: function(){
+            if(this.searchSelected){
+                this.searchSelected = false;
+            } else{
+                this.searchSelected = true;
+                this.menuSelected = false;
+            }
+        },
+    },
+
+    watch: {
+    }
 });
